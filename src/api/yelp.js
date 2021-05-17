52fc85d2d204dca525e357c2b98980ec7768a4e8
Cihat Süngü <61031932+devcihat@users.noreply.github.com>
2021-05-17 17:52:53 +0300

    Update yelp.js
    update

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
     Authorization:
-      "Bearer 2GNqRH7mpird3Mdyit553x6JnDXJCW4E6TzVfeYrASVbHMnbnM2AXni3a7SkyXiVUikU71CackyAblmuE1DEFz-ziajqbTK119pcMsZSQcnobZi7C9hXtux_oxuiYHYx",
+      "Bearer YOUR_API_KEY",
   },
 });
